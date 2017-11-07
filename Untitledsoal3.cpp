e3a35ce9c4bdf61a8718e488f198e970fb0ab05b
khairunnaji <32257830+khairunnaji@users.noreply.github.com>
2017-11-07 18:17:23 +0800

    menginput lembur gajihtotal dan panjak dan diperoleh jagih bersih

--- Untitledsoal3.cpp
+++ Untitledsoal3.cpp
@@ -9,4 +9,13 @@ int main () {
 	cin >> Gp;
 	cout << "Masukkan jam kerja    : ";
 	cin >> Jk;
+	
+	Lembur= Jk - 40;
+	Gt = (Lembur*1.5+40)+Gp;
+	Pajak=Gt*0.15;
+	Gt=Gt-Pajak;
+	
+	cout<<"Gaji bersih pegawai : "<< Gt<<endl;	
+		
 }
+
