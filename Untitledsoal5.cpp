6c121a6a4ecf16cc751013f4d41aafe85c781e66
khairunnaji <32257830+khairunnaji@users.noreply.github.com>
2017-11-07 18:54:04 +0800

    menampilkan hasil kurus atau normal

--- Untitledsoal5.cpp
+++ Untitledsoal5.cpp
@@ -9,4 +9,11 @@ int main () {
  
  cout << "Masukkan berat badan dalam kg : ";
  cin >> berat;
+ 
+ if (berat<tinggi/2.5) {
+  cout << "Hasil : Kurus " << endl;
+   }
+ else if  (tinggi/2.5 <= berat <= tinggi/2.3) {
+  cout <<  "Hasil : Normal" << endl; 
+}
 }
